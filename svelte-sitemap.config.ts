5eb5926651cfa383114f0ae7ba68928ba4277b16
toino <me@toino.pt>
2026-05-02 00:52:56 +0100

    Add sitemap

---
+++ svelte-sitemap.config.ts
@@ -0,0 +1,6 @@
+import type { OptionsSvelteSitemap } from "svelte-sitemap";
+
+export default {
+    domain: "https://luis.toino.pt",
+    outDir: ".svelte-kit/cloudflare",
+} satisfies OptionsSvelteSitemap;
