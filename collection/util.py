752d67866814abe5e0f0bab78b28677aabc01243
toino <me@toino.pt>
2026-05-01 23:10:42 +0100

    Remove old python based code

--- collection/util.py
+++
@@ -1,4 +0,0 @@
-from bs4 import BeautifulSoup
-
-def raw_text(html: str):
-    return BeautifulSoup(html, 'html.parser').get_text().replace('“', '"').replace('”', '"').replace('‘', "'").replace('’', "'")
