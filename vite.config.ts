59fea14e03513a55cd102808bf2f8bd5a8dc65ef
toino <me@toino.pt>
2026-05-02 00:54:07 +0100

    Add plugin for workflow export

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { addWorkerExports } from "@oselvar/sveltekit-add-worker-exports";
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [enhancedImages(), sveltekit(), tailwindcss()],
+    plugins: [
+        enhancedImages(),
+        sveltekit(),
+        tailwindcss(),
+        addWorkerExports({ entryPoint: "src/lib/server/worker.ts" }),
+    ],
 });
