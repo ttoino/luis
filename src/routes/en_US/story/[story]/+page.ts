9c95b134e6334ba688b7852131ca4acadf95ecd4
toino <me@toino.pt>
2026-05-15 19:12:25 +0100

    Remove en_US route

--- src/routes/en_US/story/[story]/+page.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async ({ params: { story } }) => {
-    redirect(308, `/story/${story}`);
-};
