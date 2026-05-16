0dcaf046505a9684d3217886c87b386a070fed3e
toino <me@toino.pt>
2026-05-16 02:30:54 +0100

    Basic champion and region pages

---
+++ src/routes/champion/[champion]/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import PageHeader from "$lib/components/PageHeader.svelte";
+    import { MetaTags } from "svelte-meta-tags";
+
+    let { data } = $props();
+</script>
+
+<MetaTags
+    description={data.champion.description}
+    openGraph={{
+        article: {
+            publishedTime: data.champion.releaseDate.toISOString(),
+        },
+        images: [
+            {
+                url: data.champion.image,
+            },
+        ],
+        siteName: "LUIS",
+        title: data.champion.name,
+        type: "article",
+    }}
+    title={data.champion.name}
+    titleTemplate="%s - LUIS"
+/>
+
+<PageHeader
+    date={data.champion.releaseDate}
+    image={data.champion.image}
+    subtitle={data.champion.title}
+    title={data.champion.name}
+/>
+
+<section
+    class="prose transition-[font-size,line-height] xl:prose-lg prose-img:mx-auto"
+>
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html data.champion.description}
+
+    <hr class="my-8!" />
+
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html data.champion.bio}
+</section>
---
+++ src/routes/region/[region]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import PageHeader from "$lib/components/PageHeader.svelte";
+    import { MetaTags } from "svelte-meta-tags";
+
+    let { data } = $props();
+</script>
+
+<MetaTags
+    description=""
+    openGraph={{
+        images: [
+            {
+                url: data.region.image,
+            },
+        ],
+        siteName: "LUIS",
+        title: data.region.name,
+        type: "article",
+    }}
+    title={data.region.name}
+    titleTemplate="%s - LUIS"
+/>
+
+<PageHeader image={data.region.image} title={data.region.name} />
+
+<section
+    class="prose transition-[font-size,line-height] xl:prose-lg prose-img:mx-auto"
+>
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html data.region.description}
+</section>
